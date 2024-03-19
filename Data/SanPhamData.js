669e74cbbb4780d19196fd247eedb882de042e0d
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-03-19 12:55:18 +0700

    initialize

---
+++ Data/SanPhamData.js
@@ -0,0 +1,315 @@
+var SanPhamData = [
+    {
+        id: 1,
+        ten: 'Big One',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/BigOne.png'
+    },
+    {
+        id: 2,
+        ten: 'Kenee',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/Kenee.png'
+    },
+    {
+        id: 3,
+        ten: 'Lucky',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/Lucky.png'
+    },
+    {
+        id: 4,
+        ten: 'Master Lite',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/MasterLite.png',
+    },
+    {
+        id: 5,
+        ten: 'Ruby',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/Ruby.png'
+    },
+    {
+        id: 6,
+        ten: 'Vigen Lite',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/VigenLite.png'
+    },
+    {
+        id: 7,
+        ten: 'Viogen',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/Viogen.png'
+    },
+    {
+        id: 8,
+        ten: 'Viogen Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/ViogenNộiThất.png'
+    },
+    {
+        id: 9,
+        ten: 'Viogen Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/ViogenNgoạiThất.png' 
+    },
+    {
+        id: 10,
+        ten: 'Ruby Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/RubyNộiThất.png'
+    },
+    {
+        id: 11,
+        ten: 'Ruby Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/RubyNgoạiThất.png'
+    },
+    {
+        id: 12,
+        ten: 'Big One Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/BigOneNộiThất.png'
+    }
+    ,
+    {
+        id: 13,
+        ten: 'Big One Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/BigOneNgoạiThất.png'
+    },
+    {
+        id: 14,
+        ten: 'Vigen Lite Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/VigenLiteNộiThất.png'
+    },
+    {
+        id: 15,
+        ten: 'Vigen Lite Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/VigenLiteNgoạiThất.png'
+    },
+    {
+        id: 16,
+        ten: 'Master Lite Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/MasterLiteNộiThất.png'
+    },
+    {
+        id: 17,
+        ten: 'Master Lite Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/MasterLiteNgoạiThất.png'
+    },
+    {
+        id: 18,
+        ten: 'Kenee Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/KeneeNộiThất.png'
+    },
+    {
+        id: 19,
+        ten: 'Kenee Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/KeneeNgoạiThất.png'
+    },
+    {
+        id: 20,
+        ten: 'Lucky Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/LuckyNộiThất.png'
+    },
+    {
+        id: 21,
+        ten: 'Lucky Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/LuckyNgoạiThất.png'
+    },
+    {
+        id: 22,
+        ten: 'Gold Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/GoldNộiThất.png'
+    },
+    {
+        id: 23,
+        ten: 'Gold Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/GoldNgoạiThất.png'
+    },
+    {
+        id: 24,
+        ten: 'Simon Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/SimonNộiThất.png'
+    },
+    {
+        id: 25,
+        ten: 'Simon Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/SimonNgoạiThất.png'
+    },
+    {
+        id: 26,
+        ten: 'Ogen Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/OgenNộiThất.png'
+    },
+    {
+        id: 27,
+        ten: 'Ogen Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/OgenNgoạiThất.png'
+    },
+    {
+        id: 28,
+        ten: 'Danton Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/DantonNộiThất.png'
+    },
+    {
+        id: 29,
+        ten: 'Danton Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/DantonNgoạiThất.png'
+    },
+    {
+        id: 30,
+        ten: 'Nanofa Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/NanofaNộiThất.png'
+    },
+    {
+        id: 31,
+        ten: 'Nanofa Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/NanofaNgoạiThất.png'
+    },
+    {
+        id: 32,
+        ten: 'Fostar Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/FostarNộiThất.png'
+    },
+    {
+        id: 33,
+        ten: 'Fostar Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/FostarNgoạiThất.png'
+    },
+    {
+        id: 34,
+        ten: 'Master Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/MasterNộiThất.png'
+    },
+    {
+        id: 35,
+        ten: 'Master Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/MasterNgoạiThất.png'
+    },
+    {
+        id: 36,
+        ten: 'Valtex Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/ValtexNộiThất.png'
+    },
+    {
+        id: 37,
+        ten: 'Valtex Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/ValtexNgoạiThất.png'
+    },
+    {
+        id: 38,
+        ten: 'Vigen Nội Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/VigenNộiThất.png'
+    },
+    {
+        id: 39,
+        ten: 'Vigen Ngoại Thất',
+        gia: 50000,
+        baoHanh: '',
+        loai: 'bot tret tuong',
+        hinhAnh: '../../Data/Image/VigenNgoạiThất.png'
+    }
+]
