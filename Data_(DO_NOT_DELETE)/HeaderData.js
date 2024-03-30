896d6078b91f4e5e712b99d7dfacaa5f95de5945
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-03-30 12:57:28 +0700

    finish home page

---
+++ Data_(DO_NOT_DELETE)/HeaderData.js
@@ -0,0 +1,10 @@
+var HeaderData = [
+    {
+        id: 1,
+        hinhAnh: '../../Data_(DO_NOT_DELETE)/Image/Title.png'
+    },
+    {
+        id: 2,
+        hinhAnh: '../../Data_(DO_NOT_DELETE)/Image/ScrollPanel.png'
+    }
+]
