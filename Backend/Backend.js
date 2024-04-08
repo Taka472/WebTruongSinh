9b55852a99a1c25a87b1ad103bfbcd85a29baaac
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-04-08 21:28:15 +0700

    backend

---
+++ Backend/Backend.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const sql = require('mssql');
+const cors = require('cors')
+
+var config = require('./Database/Sql.config')
+
+const port = 3001;
+
+app.use(cors())
+
+app.get('/TrangChu/getData', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err);
+        else {
+            new sql.Request().query('select * from HeaderData', (err, result) => {
+                if (err) console.log(err);
+                else {
+                    res.send(result.recordset);
+                }
+            })
+        }
+    })
+})
+
+app.get('/getCongTyData', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err);
+        else {
+            new sql.Request().query('select * from CongTyData', (err, result) => {
+                if (err) console.log(err);
+                else {
+                    res.send(result.recordset);
+                }
+            })
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log('App listening at port ' + port);
+})
---
+++ Backend/Models/HeaderData.model.js
@@ -0,0 +1,12 @@
+class HeaderData {
+    constructor (Id, ten, hinhAnh, headerType) {
+        return {
+            Id: Id,
+            ten: ten,
+            hinhAnh: hinhAnh,
+            headerType: headerType
+        }
+    }
+}
+
+module.exports = HeaderData
