01f3c2855646040666e9c99abce700166b703237
Tran Viet Phuc <nhoxphuc101@gmail.com>
2024-04-10 10:28:03 +0700

    up SP 10/04

--- WebPC/SanPham/script.js
+++ WebPC/SanPham/script.js
@@ -1,24 +1,9 @@
-let currentIndex = 0;
-const slides = document.querySelectorAll('.slider-images img');
-const totalSlides = slides.length;
-const sliderImages = document.querySelector('.slider-images');
-const slideWidth = slides[0].offsetWidth;
-let intervalId;
+document.getElementById("title").src = HeaderData[0].hinhAnh
+document.getElementById("scrollpane").src = HeaderData[1].hinhAnh
 
-function moveSlide(direction) {
-    currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
-    const offset = currentIndex * slideWidth;
-    sliderImages.style.transform = `translateX(-${offset}px)`;
-}
-
-function startSlider() {
-    intervalId = setInterval(function() {
-        moveSlide(1);
-    }, 2000); // Thay đổi 2000 thành khoảng thời gian bạn muốn mỗi hình ảnh hiển thị
-}
-
-function stopSlider() {
-    clearInterval(intervalId);
-}
-
-startSlider();
+document.getElementById("footerTitle").innerText = CongTyData.ten
+document.getElementById("infoDisplay").innerHTML += "<li>Trụ sở: " + CongTyData.diaChi;
+document.getElementById("infoDisplay").innerHTML += "<li>Chi nhánh: " + CongTyData.chiNhanh;
+document.getElementById("infoDisplay").innerHTML += "<li>Tel: " + CongTyData.sdt[0] + " - " + CongTyData.sdt[1] + " Hotline: " + CongTyData.hotline;
+document.getElementById("infoDisplay").innerHTML += "<li>Giấy phép kinh doanh số: " + CongTyData.giayPhepKinhDoanh;
+document.getElementById("infoDisplay").innerHTML += "<li>Copyright 2018 © Bản quyền thuộc về Công ty"
