ec3f4aba7c36bbce04e12c08d57a20b0cf16bdd0
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-04-11 20:54:22 +0700

    pushing server and backend

---
+++ Backend/Backend.js
@@ -0,0 +1,145 @@
+const express = require('express');
+const app = express();
+const sql = require('mssql');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const secretKey = 'f8cdb04495ded47615258f9dc6a3f4707fd2405434fefc3cbf4ef4e6';
+
+var config = require('./Database/Sql.config')
+
+const port = 3001;
+
+app.use(cors())
+app.use(express.json())
+
+const authenticateJWT = (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).json({ message: 'Authenticate failed'});
+
+    try {
+        const decode = jwt.verify(token, secretKey);
+        req.user = decode;
+        next();
+    } catch (err) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
+}
+
+app.get('/api/TrangChu/getHeaderData', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err);
+        else {
+            new sql.Request().query('select * from HeaderData', (err, result) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.status(200).send(result.recordset);
+                }
+            })
+        }
+    })
+})
+
+app.get('/api/getCongTyData', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err);
+        else {
+            new sql.Request().query('select * from CongTyData', (err, result) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.status(200).send(result.recordset);
+                }
+            })
+        }
+    })
+})
+
+app.get('/api/getChungNhanData', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err);
+        else {
+            new sql.Request().query('select * from ChungNhanData', (err, result) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.status(200).send(result.recordset);
+                }
+            })
+        }
+    })
+})
+
+app.get('/api/getCongTrinhTieuBieu', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err)
+        else {
+            new sql.Request().query('select * from CongTrinhTieuBieu', (err, result) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.status(200).send(result.recordset)
+                }
+            })
+        }
+    })
+})
+
+app.get('/api/getSanPhamData', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err)
+        else {
+            new sql.Request().query('select * from SanPhamData', (err, result) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.status(200).send(result.recordset)
+                }
+            })
+        }
+    })
+})
+
+app.get('/api/getTinTucData', (req, res) => {
+    sql.connect(config, function(err) {
+        if (err) console.log(err)
+        else {
+            new sql.Request().query('select * from TinTucData', (err, result) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.status(200).send(result.recordset)
+                }
+            })
+        }
+    })
+})
+
+app.put('/api/putForm', (req, res) => {
+    console.log(req.body)
+    sql.connect(config, function(err) {
+        if (err) console.log(err)
+        else {
+            new sql.Request().query("insert into FormCuaNguoiDung values ()", (err) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).send(err)
+                }
+                else {
+                    console.log("Success")
+                    res.status(200).send({ message: 'insert success' })
+                }
+            })
+        }
+    })
+})
+
+app.listen(port, () => {
+    console.log('App listening at port ' + port);
+})
