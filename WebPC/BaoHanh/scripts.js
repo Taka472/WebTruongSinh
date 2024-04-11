28847a0728fae1233bec687121548c09fd4cabb5
minnhhnngoc <thanhngocbuiminh@gmail.com>
2024-04-11 13:07:44 +0700

    update BaoHanh

--- WebPC/BaoHanh/scripts.js
+++ WebPC/BaoHanh/scripts.js
@@ -1,12 +1,84 @@
-document.getElementById("title").src = HeaderData[0].hinhAnh
-document.getElementById("scrollpane").src = HeaderData[1].hinhAnh
+let HeaderData, CongTyData, ChungNhanData;
 
-document.getElementById("firstCer").src = ChungNhanData[0].hinhAnh
-document.getElementById("secondCer").src = ChungNhanData[1].hinhAnh
+const apiURL = 'http://localhost:3001'
+const serverURL = 'http://localhost:3000'
 
-document.getElementById("footerTitle").innerText = CongTyData.ten
-document.getElementById("infoDisplay").innerHTML += "<li>Trụ sở: " + CongTyData.diaChi;
-document.getElementById("infoDisplay").innerHTML += "<li>Chi nhánh: " + CongTyData.chiNhanh;
-document.getElementById("infoDisplay").innerHTML += "<li>Tel: " + CongTyData.sdt[0] + " - " + CongTyData.sdt[1] + " Hotline: " + CongTyData.hotline;
-document.getElementById("infoDisplay").innerHTML += "<li>Giấy phép kinh doanh số: " + CongTyData.giayPhepKinhDoanh;
-document.getElementById("infoDisplay").innerHTML += "<li>Copyright 2018 © Bản quyền thuộc về Công ty"
+const makeAPICall = async() => {
+    try {
+        await fetch(apiURL + '/TrangChu/getHeaderData', {mode: 'cors'})
+        .then(response => response.json())
+        .then(data => {
+            HeaderData = data
+            return HeaderData
+        })
+        .then (() => {
+            document.getElementById("title").src = serverURL + HeaderData[0].hinhAnh
+            document.getElementById("scrollpane").src = serverURL + HeaderData[1].hinhAnh;
+        })
+        .catch(error => console.error('Error:', error));
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const fecthCongTyData = async() => {
+    try {
+        await fetch(apiURL + '/getCongTyData', {mode: 'cors'})
+        .then(response => response.json())
+        .then(data => {
+            CongTyData = data;
+            return CongTyData
+        })
+        .then (() => {
+            document.getElementById("footerTitle").innerText = CongTyData[0].ten
+            document.getElementById("infoDisplay").innerHTML += "<li>Trụ sở: " + CongTyData[0].diaChi;
+            document.getElementById("infoDisplay").innerHTML += "<li>Chi nhánh: " + CongTyData[0].chiNhanh;
+            document.getElementById("infoDisplay").innerHTML += "<li>Tel: " + CongTyData[0].sdt1 + " - " + CongTyData[0].sdt2 + " Hotline: " + CongTyData[0].hotline;
+            document.getElementById("infoDisplay").innerHTML += "<li>Giấy phép kinh doanh số: " + CongTyData[0].giayPhepKinhDoanh;
+            document.getElementById("infoDisplay").innerHTML += "<li>Copyright 2018 © Bản quyền thuộc về Công ty"
+        })
+        .catch(error => console.error('Error:', error));
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+makeAPICall()
+fecthCongTyData()
+
+var SanPham = document.getElementById('sanPham')
+var ChungNhan = document.getElementById('chungNhan')
+var TrangChu = document.getElementById('trangChu')
+var HuongDanSuDung = document.getElementById('huongDanSuDung')
+var CongTrinhTieuBieu = document.getElementById('congTrinhTieuBieu')
+var TinTuc = document.getElementById('last')
+var LienHe = document.getElementById('chatbox')
+
+SanPham.onclick = function() {
+    window.location = '/SanPham'
+}
+
+TrangChu.onclick = function() {
+    window.location = '/TrangChu'
+}
+
+ChungNhan.onclick = function() {
+    window.location = '/ChungNhan'
+}
+
+HuongDanSuDung.onclick = function() {
+    window.location = '/HuongDanSuDung'
+}
+
+CongTrinhTieuBieu.onclick = function() {
+    window.location = '/CongTrinhTieuBieu'
+}
+
+TinTuc.onclick = function() {
+    window.location = '/TinTuc'
+}
+
+LienHe.onclick = function() {
+    window.location = '/LienHe'
+}
